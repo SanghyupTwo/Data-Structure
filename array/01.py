c9c26bb98efb96e810e651bc1dbb25ef303f82bb
SanghyupTwo <dwc07238@naver.com>
2024-02-17 21:47:50 +0900

    Fix incorrect solution for problem 01.py

--- array/01.py
+++ array/01.py
@@ -1,2 +1,9 @@
 def solution(arr):
-    return arr.sort()
+    arr.sort()
+    return arr
+
+
+# return에서 바로 arr.sort()를 하면 None이 출력
+# return에 바로 하려면 sorted(arr) 사용
+
+print(solution([1, -5, 2, 4, 3]))
